962644346ff0feebea3368ba000f2e2d8528bbe3
mr Anyway <simonstephano328@gmail.com>
2025-03-20 08:19:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
+
+// Set default max listeners if necessary
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Body parser middleware comes before routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/qr', server);
+app.use('/code', code);
+
+// Serve pair.html file
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(path.join(process.cwd(), 'pair.html'));
+});
+
+// Serve main.html file
+app.use('/', async (req, res, next) => {
+    res.sendFile(path.join(process.cwd(), 'main.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:` + PORT)
-})
+});
 
-module.exports = app
+module.exports = app;
